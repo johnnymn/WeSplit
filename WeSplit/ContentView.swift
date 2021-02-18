64636dfbf06c29b8d75f29f93a7dbfe7e67bfd7f
Johnny Martinez <johnnymn@hotmail.com>
2021-02-17 21:15:48 -0800

    Add structure and state to the ContentView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,12 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+  // The @State property wrapper
+  // allows us to associate state
+  // to an immutable view struct.
+  @State private var tapCount = 0
+
   var body: some View {
-    Text("Hello, world!")
-            .padding()
+    // Place a navigation at
+    // the top of the screen.
+    NavigationView {
+      // Forms are scrolling lists
+      // of static controls.
+      Form {
+        Section {
+          Text("Hello World!")
+        }
+      }.navigationBarTitle("Swift")
+    }
+
+    // A button that mutates state.
+    Button("Tap Count: \(tapCount)") {
+      self.tapCount += 1
+    }
   }
 }
 
+// Here we can customize the preview
+// that will be show on Xcode's canvas.
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
